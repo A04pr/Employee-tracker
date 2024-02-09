d5a4480741f7206156c16578551a46964c47d04d
A04pr <areed041704@gmail.com>
2024-02-08 18:13:07 -0700

    added info to sql files

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (department_name)
+VALUES ("Lift Ops"),
+       ("Food & Beverage"),
+       ("Hospitality"),
+       ("Medical");
+
+INSERT INTO role (title, salary)
+VALUES ("Lift Operator", 40000),
+       ("Head Chef", 75000),
+       ("Line Cook", 45000),
+       ("Janitor", 50000),
+       ("Hospitality Admin", 80000),
+       ("Nurse", 60000),
+       ("Doctor", 120000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Tracy", "Calvins"),
+       ("Kalli", "Cera"),
+       ("Kevin", "Star"),
+       ("Maya", "Viselli"),
+       ("Boris", "Konev"),
+       ("Tyler", "Steinkamp"),
+       ("Sien", "Ossef"),
+       ("Zarayah", "Jordan");
+       
